0266883a12fb427416a6825c29a026726629301d
Trav <travis.vas@gmail.com>
2024-07-18 16:45:29 -0400

    added streamlit

--- main.py
+++ main.py
@@ -1,29 +1,46 @@
-#%%
-# Import libraries
+import streamlit as st
+import streamlit_shadcn_ui as sui
+from pygwalker.api.streamlit import StreamlitRenderer
+import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-#%%
-# Defining data for the dataframe
-data = {
-    'Basket': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P'],
-    'Apples': [10, 20, 30, 56, 40, 40, 67, 47, 40, 4, 49, 52, 5, 56, 35, 45],
-    'Bananas': [15, 6, 3, 45, 67, 44, 45, 11, 14, 18, 13, 12, 1, 34, 12, 12]
-}
 
-# Creating the dataframe
-df = pd.DataFrame(data)
+st.title("RiseNow Intelligent Data Inspector")
 
-df
-#%%
-# Calculate the sums
-sum_apples = df['Apples'].sum()
-sum_bananas = df['Bananas'].sum()
 
-# Create a bar chart
-plt.bar(['Apples', 'Bananas'], [sum_apples, sum_bananas], color=['red', 'blue'])
+@st.cache_data
+def clean_messy_csv(file_path):
+    """
+    Reads a CSV file with tab-separated values, cleans it, and returns a pandas DataFrame.
 
-# Set a title
-plt.title('Comparison of total Apples and Bananas')
+    Parameters:
+    file_path (str): The path to the CSV file.
 
-# Show the plot
-plt.show()
+    Returns:
+    pd.DataFrame: Cleaned data as a pandas DataFrame.
+    """
+    # Read the file as a text file to manually process the lines
+    with open(
+        file_path, "r", encoding="utf-8"
+    ) as file:  # Added encoding to handle potential UnicodeDecodeErrors
+        lines = file.readlines()
+
+    # Split the header and data lines
+    header = lines[0].strip().split("\t")
+    data_lines = [line.strip().split("\t") for line in lines[1:]]
+
+    # Create a DataFrame from the processed data
+    cleaned_data = pd.DataFrame(data_lines, columns=header)
+
+    # Remove any leading/trailing whitespace characters from the headers
+    cleaned_data.columns = cleaned_data.columns.str.strip()
+
+    # Optionally, remove any rows with entirely empty values
+    cleaned_data.dropna(how="all", inplace=True)
+
+    return cleaned_data
+
+
+# Now, let's use the function and display the first few rows of the cleaned DataFrame
+clean_data = clean_messy_csv("data/Spend_Intake_010124_063024.csv")
+pyg_app = StreamlitRenderer(clean_data)
+pyg_app.explorer()
